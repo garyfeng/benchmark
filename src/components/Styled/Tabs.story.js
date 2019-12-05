5c4efe2266a21655bf433be55128e6de0afb74bd
Corey M <corey.mac@gmail.com>
2019-12-05 14:30:48 +1100

    Add Tabs component

---
+++ src/components/Styled/Tabs.story.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import Global from './Global.js';
+import ThemeProvider from './ThemeProvider.js';
+
+import { Tabs, Tab, TabsList } from './Tabs.js';
+import { Box, Flex } from './Box.js';
+
+storiesOf('Styled System', module).add('Tabs ', () => {
+  let [selectedTab, setSelectedTab] = useState(0);
+
+  return (
+    <>
+      <Global />
+      <ThemeProvider>
+        <Flex flexDirection="column" width="100%">
+          <Box>
+            <Tabs activeTab={selectedTab} onChange={setSelectedTab}>
+              <TabsList>
+                <Tab>1</Tab>
+                <Tab>2</Tab>
+                <Tab>3</Tab>
+                <Tab>4</Tab>
+                <Tab>5</Tab>
+                <Tab>Review</Tab>
+              </TabsList>
+            </Tabs>
+          </Box>
+        </Flex>
+      </ThemeProvider>
+    </>
+  );
+});
